f8435baa48c02dc0140ab36bdff45849d3c9bab5
Nick Shelley <nickmshelley@gmail.com>
2019-04-11 09:00:37 -0600

    Basic search view.

---
+++ FlickrSearch/SearchViewController.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    let searchBar = UISearchBar(frame: .zero)
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            searchBar.bottomAnchor.constraint(equalTo: tableView.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+        
+        searchBar.delegate = self
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(searchBar.text)
+    }
+}
